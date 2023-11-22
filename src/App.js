0dc6d036c0492f71d3ee3296c6d343c9473d9d1a
IrfanCatovic <catovicc84@gmail.com>
2023-11-22 17:05:52 +0100

    Shopping cart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
-function App() {
+const shopingItems = [
+  {
+    name: "Water",
+    price: 350,
+    payed: true,
+    discount: 10,
+    amount: 2,
+  },
+  {
+    name: "Bread",
+    price: 50,
+    payed: false,
+    discount: 0,
+    amount: 1,
+  },
+  {
+    name: "Coffee",
+    price: 100,
+    payed: true,
+    discount: 15,
+    amount: 5,
+  },
+];
+
+export default function App() {
+  const [allItems, setAllItems] = useState([...shopingItems]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ItemsList shopingItems={shopingItems} />
     </div>
   );
 }
 
-export default App;
+function ItemsList({ shopingItems }) {
+  return (
+    <div>
+      <ul className="list">
+        {shopingItems.map((item) => (
+          <Item item={item} />
+        ))}
+        <button>Add new</button>
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  const total = (item.price - (item.price * item.discount) / 100) * item.amount;
+  const [isPayed, setIsPayed] = useState(false);
+
+  function onToggleItem(id) {
+    setIsPayed((allItems) =>
+      allItems.map((item) =>
+        item.id === id ? { ...item, payed: !item.payed } : item
+      )
+    );
+  }
+
+  return (
+    <div
+      className="card"
+      style={item.payed ? { backgroundColor: "green", color: "#fff" } : null}
+    >
+      <p>{`Name: ${item.name}`}</p>
+      <p>{`Price: ${item.price}`}</p>
+      <p>{`Discount: ${item.discount}%`}</p>
+      <p>{`Amount: ${item.amount}`}</p>
+      <p>{`Total to pay: ${total}`}</p>
+      <input
+        type="checkbox"
+        value={item.payed}
+        onChange={() => {
+          onToggleItem(item.id);
+        }}
+      />
+    </div>
+  );
+}
