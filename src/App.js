687900b0e6ce3886be64737094523d9ca466cddd
IrfanCatovic <catovicc84@gmail.com>
2023-11-23 01:50:03 +0100

    Form works

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ const shopingItems = [
     payed: true,
     discount: 10,
     amount: 2,
+    id: 1,
   },
   {
     name: "Bread",
@@ -15,6 +16,7 @@ const shopingItems = [
     payed: false,
     discount: 0,
     amount: 1,
+    id: 2,
   },
   {
     name: "Coffee",
@@ -22,61 +24,94 @@ const shopingItems = [
     payed: true,
     discount: 15,
     amount: 5,
+    id: 3,
   },
 ];
 
 export default function App() {
-  const [allItems, setAllItems] = useState([...shopingItems]);
+  const [allItems, setAllItems] = useState([]);
+
+  function handleAddCards(item) {
+    setAllItems([...allItems, item]);
+  }
 
   return (
     <div className="App">
-      <ItemsList shopingItems={shopingItems} />
+      <button>Add</button>
+      <ItemsList
+        allItems={allItems}
+        shopingItems={shopingItems}
+        onAddCards={handleAddCards}
+      />
+      <AddFrom onAddCards={handleAddCards} />
     </div>
   );
 }
 
-function ItemsList({ shopingItems }) {
+function ItemsList({ allItems }) {
   return (
-    <div>
-      <ul className="list">
-        {shopingItems.map((item) => (
-          <Item item={item} />
-        ))}
-        <button>Add new</button>
-      </ul>
+    <div className="list">
+      {allItems.map((item) => (
+        <Item item={item} key={item.id} />
+      ))}
     </div>
   );
 }
 
 function Item({ item }) {
   const total = (item.price - (item.price * item.discount) / 100) * item.amount;
-  const [isPayed, setIsPayed] = useState(false);
-
-  function onToggleItem(id) {
-    setIsPayed((allItems) =>
-      allItems.map((item) =>
-        item.id === id ? { ...item, payed: !item.payed } : item
-      )
-    );
-  }
 
   return (
     <div
       className="card"
       style={item.payed ? { backgroundColor: "green", color: "#fff" } : null}
     >
-      <p>{`Name: ${item.name}`}</p>
-      <p>{`Price: ${item.price}`}</p>
-      <p>{`Discount: ${item.discount}%`}</p>
-      <p>{`Amount: ${item.amount}`}</p>
-      <p>{`Total to pay: ${total}`}</p>
-      <input
-        type="checkbox"
-        value={item.payed}
-        onChange={() => {
-          onToggleItem(item.id);
-        }}
-      />
+      <div className="card-content">
+        <p>{`Name: ${item.name}`}</p>
+        <p>{`Price: ${item.price}`}</p>
+        <p>{`Discount: ${item.discount}%`}</p>
+
+        <span>Choose amount </span>
+        <input type="number" />
+
+        <p>{`Total to pay: ${total}`}</p>
+        <button>Buy</button>
+      </div>
+    </div>
+  );
+}
+
+function AddFrom({ onAddCards }) {
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [discount, setDiscount] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    const newItem = { name, price, discount, id: Date.now() };
+
+    onAddCards(newItem);
+  }
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h3>Add new item shop</h3>
+        <input
+          placeholder="Enter name"
+          type="text"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          placeholder="Enter price"
+          type="text"
+          onChange={(e) => setPrice(Number(e.target.value))}
+        />
+        <input
+          type="text"
+          placeholder="Enter discount"
+          onChange={(e) => setDiscount(Number(e.target.value))}
+        />
+        <button type="submit">Add item</button>
+      </form>
     </div>
   );
 }
